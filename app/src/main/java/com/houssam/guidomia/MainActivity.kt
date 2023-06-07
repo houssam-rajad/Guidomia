6d83c88fc8c6334f7b311a1c8b0a75aa212ecd5a
Houssam Rajad <rajad.houssam@ibm.com>
2023-06-07 01:52:13 +0100

    H-1: adding resources and Creating UI Classes with their Layouts and setting Navigation Component

--- app/src/main/java/com/houssam/guidomia/MainActivity.kt
+++ app/src/main/java/com/houssam/guidomia/MainActivity.kt
@@ -2,12 +2,22 @@ package com.houssam.guidomia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.houssam.guidomia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBinding()
+    }
+
+    private fun setBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
     }
 }
